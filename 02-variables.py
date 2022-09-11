7eeeff6d6d37ada8bfaa2c9c21cd3e9a6c0b0016
MikeOxMedium <111542535+MikeOxMedium@users.noreply.github.com>
2022-09-12 09:48:52 +1200

    Add files via upload

--- 02-variables.py
+++ 02-variables.py
@@ -8,3 +8,5 @@ fav_num = 7
 
 #below is a constant (the name is an UPPERCASE)
 PI = 3.14
+
+num_2 = 2
---
+++ 03_getting_input.py
@@ -0,0 +1,15 @@
+# Ask the user for their name
+username = input("what is your name?")
+
+# Ask the user for their favorite interger
+fav_num = int(input("Favourite Number? "))
+
+# Double, halve and square the number
+double = fav_num * 2
+half = fav_num / 2
+squared = fav_num * fav_num
+
+# Greet the user
+
+# Output the results of doubling, halving
+# and squaring their favorite number 
